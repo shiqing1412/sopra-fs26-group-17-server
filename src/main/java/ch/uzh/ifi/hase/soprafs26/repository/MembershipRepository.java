e79272b0c3772a4c5b50e40402a7ed52811af937
Julia <juliaahoop@gmail.com>
2026-04-07 10:58:12 +0200

    Fix MembershipRepository missing existsByTripAndUser method (#97)

--- src/main/java/ch/uzh/ifi/hase/soprafs26/repository/MembershipRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/repository/MembershipRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import ch.uzh.ifi.hase.soprafs26.entity.Membership;
+import ch.uzh.ifi.hase.soprafs26.entity.Trip;
+import ch.uzh.ifi.hase.soprafs26.entity.User;
 import java.util.Optional;
 import ch.uzh.ifi.hase.soprafs26.entity.Trip;
 import ch.uzh.ifi.hase.soprafs26.entity.User;
@@ -18,4 +20,6 @@ public interface MembershipRepository extends JpaRepository<Membership, Long> {
 
     @Query("SELECT COUNT(m) > 0 FROM Membership m WHERE m.trip.tripId = :tripId AND m.user.id = :userId")
     boolean existsByTripIdAndUserId(@Param("tripId") Long tripId, @Param("userId") Long userId);
+
+    boolean existsByTripAndUser(Trip trip, User user);
 }
