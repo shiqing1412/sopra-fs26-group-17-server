c979814840fb7aa64a76f9504410d27500759261
Julia <juliaahoop@gmail.com>
2026-04-11 11:00:08 +0200

    Restrict access to authenticated trip members only (#98)

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/controller/EventController.java
@@ -0,0 +1,34 @@
+package ch.uzh.ifi.hase.soprafs26.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import ch.uzh.ifi.hase.soprafs26.entity.User;
+import ch.uzh.ifi.hase.soprafs26.rest.dto.DayDTO;
+import ch.uzh.ifi.hase.soprafs26.service.EventService;
+import ch.uzh.ifi.hase.soprafs26.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/trips")
+public class EventController {
+  private final EventService eventService;
+  private final UserService userService;
+
+  public EventController(EventService eventService, UserService userService) {
+    this.eventService = eventService;
+    this.userService = userService;
+  }
+
+  @GetMapping("/{tripId}/events")
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+  public List<DayDTO> getEventsGroupedByDay(
+        @PathVariable Long tripId,
+        @RequestHeader("Authorization") String token) {
+
+    User requestingUser = userService.validateToken(token);
+    return eventService.getEventsGroupedByDay(tripId, requestingUser);
+  }
+}
