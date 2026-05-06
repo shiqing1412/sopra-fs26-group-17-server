30652e0f9558c007ccdcad5919db1e9b8701b7a9
dsgji4g4 <i-hsin.lin@uzh.ch>
2026-05-06 12:55:11 +0200

    Implement global exception handling for validation and server errors(#237)

--- src/main/java/ch/uzh/ifi/hase/soprafs26/service/TripService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/service/TripService.java
@@ -6,8 +6,6 @@ import java.util.UUID;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,7 +27,6 @@ import ch.uzh.ifi.hase.soprafs26.rest.dto.TripPreviewDTO;
 @Transactional
 public class TripService {
     
-    private final Logger log = LoggerFactory.getLogger(TripService.class);
     private final TripRepository tripRepository;
     private final MembershipRepository membershipRepository;
     private final EventRepository eventRepository;
